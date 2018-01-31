0b20ab54cd2f65c0f223e6507efd75184f9f442a
Dale Pollitt <dale.negativezero@gmail.com>
2018-01-31 18:12:18 +0000

    remove fuzzy hash feature

--- app.rb
+++ app.rb
@@ -59,40 +59,4 @@ post '/random' do
     @alphabet = Helper.randomize_alphabet
     erb :'index.html', { layout: :'layout.html' }
 end
-
-get '/fuzzy-hash' do
-    @result = false
-    erb :'fuzzy-hash.html', { layout: :'layout.html' } 
-end
-
-post '/fuzzy-hash' do
-    
-    if params[:file1] and params[:file2]
-        file1 = params[:file1][:tempfile]
-        file2 = params[:file2][:tempfile]
-        
-        begin
-    
-            hash1 = `ssdeep -b #{file1.path} > hashes.txt`
-            @result = `ssdeep -b -m hashes.txt #{file2.path}`
-            
-            if @result.empty?
-                @result = "0"
-            else
-                @result = @result.slice(-7..-2).gsub(/[a-zA-Z() ]/, '') if @result
-            end
-
-        ensure
-        
-            file1.close!
-            file2.close!
-        end
-        
-        erb :'fuzzy-hash.html', { layout: :'layout.html' } 
-    else
-        flash.now[:error] = "Make sure that you have entered files in both fields."
-        erb :'fuzzy-hash.html', { layout: :'layout.html' } 
-    end
-    
-end
     
